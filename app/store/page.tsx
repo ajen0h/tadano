223ef3f041d59a7d1353d1f15c486d5e5367331a
ElAjen0 <sauldevrp@gmail.com>
2024-02-02 16:18:40 +0100

    navbar created

--- app/store/page.tsx
+++ app/store/page.tsx
@@ -32,7 +32,7 @@ const StorePage = async () => {
   return (
     <div>
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-        {products.map((product) => (
+        {products.map((product:any) => (
           <ProductCard key={product.id} product={product} />
         ))}
         <h1>Hola</h1>
